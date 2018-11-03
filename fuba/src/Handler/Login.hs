d68fd1bc6c0783b9f76d3a00f408be4f4485a31d
adaao <armascarelli@gmail.com>
2018-11-03 04:29:35 +0000

    routing - narf

--- fuba/src/Handler/Login.hs
+++ fuba/src/Handler/Login.hs
@@ -14,54 +14,57 @@ formLogin :: Form (Text,Text)
 formLogin = renderDivs $ (,) 
     <$> areq emailField "Email: " Nothing
     <*> areq passwordField "Senha: " Nothing
-
+{-
 postLoginnR :: Handler Html
 postLoginnR = undefined
 
 getLoginnR :: Handler Html
 getLoginnR = undefined
-
-{-   
-getLoginR :: Handler Html
-getLoginR = do 
+-}
+   
+getLoginnR :: Handler Html
+getLoginnR = do 
     (widget,enctype) <- generateFormPost formLogin
     mensa <- getMessage
     defaultLayout $ do 
         [whamlet|
             $maybe msg <- mensa
                 ^{msg}
-            <form action=@{LoginR} method=post>
+            <form action=@{LoginnR} method=post>
                 ^{widget}
                 <input type="submit" value="Login">
         |]
 
-autentica :: Text -> Text -> HandlerT App IO (Maybe (Entity Cliente))
-autentica email senha = runDB $ selectFirst [ClienteEmail ==. email
-                                            ,ClienteSenha ==. senha] []
+autentica :: Text -> Text -> HandlerT App IO (Maybe (Entity User))
+autentica email password = runDB $ selectFirst [UserEmail ==. email
+                                               ,UserPassword ==. password] []
 
-postLoginR :: Handler Html
-postLoginR = do 
-    ((resultado,_),_) <- runFormPost formLogin
-    case resultado of
+postLoginnR :: Handler Html
+postLoginnR = do 
+    ((result,_),_) <- runFormPost formLogin
+    case result of
         FormSuccess ("root@root.com","root") -> do 
             setSession "_NOME" "admin"
-            redirect ClienteR
+            redirect 
         FormSuccess (email,senha) -> do 
-            talvezCliente <- autentica email senha
-            case talvezCliente of 
+            maybeUser <- autentica email senha
+            case maybeUser of 
                 Nothing -> do 
                     setMessage [shamlet|
                         <div> 
                             Usuario nao encontrado/Senha invalida!
                     |]
-                    redirect LoginR
-                Just (Entity _ cli) -> do 
-                    setSession "_NOME" (clienteNome cli)
+                    redirect LoginnR
+                Just (Entity _ usr) -> do 
+                    setSession "_NOME" (userName usr)
                     redirect HomeR
-            redirect ClienteR
+            redirect 
         _ -> redirect HomeR
--}
+
 postLogoutR :: Handler Html
 postLogoutR = do 
     deleteSession "_NOME"
     redirect HomeR
+
+-- getMessage :: Text
+-- getMessage = "Usuário não encontrado ou senha inválida!"
